4bbe7291dbae18414daf32493569fbb805d6a006
livramatheus <77751193+livramatheus@users.noreply.github.com>
2021-10-17 10:55:05 -0300

    Added project screenshots

--- src/services/previsao.js
+++ src/services/previsao.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export function getPrevisao(latitude, longitude) {
-  return Axios.get("https://lcmfv.sse.codesandbox.io/previsao", {
+  return Axios.get("https://z8nuo.sse.codesandbox.io/previsao", {
     params: {
       lat: latitude,
       long: longitude
